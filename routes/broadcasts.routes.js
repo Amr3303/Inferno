507ab990a66d03daf6353f22c5812904020cd602
Amr Khaled <amr287491@gmail.com>
2025-04-19 02:07:40 +0200

    Deleted a redundant line
    - With the help of SHOKRY
    
    Co-Authored-By: Abdelrahman-shokry556 <145803891+Abdelrahman-shokry556@users.noreply.github.com>

--- routes/broadcasts.routes.js
+++ routes/broadcasts.routes.js
@@ -17,7 +17,6 @@ router.post("/", auth, createBroadcast);
 router.get("/", auth, getAllBroadcasts);
 router.get("/unjoined", auth, getUnjoinedBroadcasts);
 router.get("/my", auth, getMyBroadcasts);
-router.get("/my", auth, getMyBroadcasts);
 router.get("/:id", auth, getSingleBroadcast);
 router.post("/:id/join", auth, joinBroadcast);
 router.delete("/deleteAllBroadcasts", deleteAllBroadcasts);
